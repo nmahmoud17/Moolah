ed719585eb376dbab1a09869ac92105daf87119a
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-03-11 15:05:54 -0400

    Adds properties to CurrencyData class with accessor methods.

--- src/main/java/com/detroitlabs/moolahapp/Model/CurrencyData.java
+++ src/main/java/com/detroitlabs/moolahapp/Model/CurrencyData.java
@@ -1,4 +1,26 @@
 package com.detroitlabs.moolahapp.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CurrencyData {
+
+    private double USDAED;
+
+
+    public double exchangeUSDollars(double USDollars){
+        return USDollars * getUSDAED();
+    }
+
+    @JsonProperty("USDAED")
+    public double getUSDAED() {
+        return USDAED;
+    }
+
+    @JsonProperty("USDAED")
+    public void setUSDAED(double USDAED) {
+        this.USDAED = USDAED;
+    }
+
 }
