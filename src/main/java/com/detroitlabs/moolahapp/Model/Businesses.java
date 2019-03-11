5dd92ce76bc7063bc1fc4c5e2e05acf9e0927bc0
NM17 <nada.mahmoud@detroitlabs.com>
2019-03-11 16:32:26 -0400

    adds ArrayList of Business information class, and adds accesssor methods

--- src/main/java/com/detroitlabs/moolahapp/Model/Businesses.java
+++ src/main/java/com/detroitlabs/moolahapp/Model/Businesses.java
@@ -1,4 +1,21 @@
 package com.detroitlabs.moolahapp.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Businesses {
+
+    @JsonProperty("businesses")
+    private ArrayList<BusinessInformation> allYelpData = new ArrayList<>();
+
+    public ArrayList<BusinessInformation> getAllYelpData() {
+        return allYelpData;
+    }
+
+    public void setAllYelpData(ArrayList<BusinessInformation> allYelpData) {
+        this.allYelpData = allYelpData;
+    }
 }
