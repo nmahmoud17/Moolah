a517551a6bd4a021247cc23a9d69f334488651c3
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-03-13 11:18:25 -0400

    Adds method in controller to successfully show data from the CurrencyLayerAPI to our home page

--- src/main/java/com/detroitlabs/moolahapp/Controller/CurrencyController.java
+++ src/main/java/com/detroitlabs/moolahapp/Controller/CurrencyController.java
@@ -1,5 +1,28 @@
 package com.detroitlabs.moolahapp.Controller;
 
+import com.detroitlabs.moolahapp.Model.AllCurrencyData;
+import com.detroitlabs.moolahapp.Services.CurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class CurrencyController {
+
+    @Autowired
+    CurrencyService currencyService;
+
+    @RequestMapping("/")
+    public ModelAndView showCurrencyInformation(){
+        ModelAndView modelAndView = new ModelAndView("home");
+       AllCurrencyData allCurrencyData = currencyService.fetchCurrencyInfo();
+       Double usdToAud= allCurrencyData.getQuotes().getUSDAUD();
+       modelAndView.addObject("currency"
+               , usdToAud);
+
+        return modelAndView;
+    }
 
 }
