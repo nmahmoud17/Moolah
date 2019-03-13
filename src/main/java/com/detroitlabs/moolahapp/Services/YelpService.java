2bcf62b9727a42d8538c9c5c2c32689957595a53
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-03-13 14:42:23 -0400

    Adds logic to hook up the Yelp API and a test to check if it is returning the correct value for the name of the first item returned, Ernie's Market

--- src/main/java/com/detroitlabs/moolahapp/Services/YelpService.java
+++ src/main/java/com/detroitlabs/moolahapp/Services/YelpService.java
@@ -1,24 +1,30 @@
 package com.detroitlabs.moolahapp.Services;
 
 import com.detroitlabs.moolahapp.Model.Businesses;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
 
 @Component
 public class YelpService {
 
-    @Value("yelpToken")
-    String yelpAPIKey;
 
     public Businesses fetchYelpData(){
-    Businesses b = new Businesses();
+
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization",  yelpAPIKey);
+        headers.add("Authorization", "BEARER iRtbrQng3VXivm-1VrAHkh5DPWQOomj9raedfqWflsFAUlF4SCHE9fUHuwS-8tnegHzLVKsGTRoH49nWsULC2lvSaonzzoOIY6UuBvO4YijJ1xmpHSmbb-_1qX2GXHYx");
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
 
-        return b;
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<Businesses> responseEntity =
+                restTemplate.exchange("https://api.yelp.com/v3/businesses/search?sort_by=rating&location=detroit,mi"
+                        , HttpMethod.GET, httpEntity, Businesses.class);
+
+        return responseEntity.getBody();
+
     }
 }
