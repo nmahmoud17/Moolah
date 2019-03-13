86abf90ee4e9c686af1daff8343086cb01a5b78b
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-03-13 11:04:02 -0400

    Added rest template to connect to API in currency service and wrote a test method to ensure that the response code is 200

--- src/main/java/com/detroitlabs/moolahapp/Services/CurrencyService.java
+++ src/main/java/com/detroitlabs/moolahapp/Services/CurrencyService.java
@@ -1,4 +1,18 @@
 package com.detroitlabs.moolahapp.Services;
 
+import com.detroitlabs.moolahapp.Model.AllCurrencyData;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
 public class CurrencyService {
+
+    public AllCurrencyData fetchCurrencyInfo(){
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject("http://www.apilayer.net/"+
+                "api/live?"+
+                "access_key=b679b47cee24c4f4863af866d04e9108", AllCurrencyData.class);
+
+
+    }
 }
