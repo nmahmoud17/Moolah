d4c04d649f8cccb7c055434b5ec8c4a2b2b079bd
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-03-13 11:59:53 -0400

    Adds logic to hide API Key for Yelp and building the API call

--- src/main/java/com/detroitlabs/moolahapp/Model/AllYelpData.java
+++
@@ -1,5 +0,0 @@
-package com.detroitlabs.moolahapp.Model;
-
-public class AllYelpData {
-
-}
--- src/main/java/com/detroitlabs/moolahapp/Services/YelpService.java
+++ src/main/java/com/detroitlabs/moolahapp/Services/YelpService.java
@@ -1,4 +1,24 @@
 package com.detroitlabs.moolahapp.Services;
 
+import com.detroitlabs.moolahapp.Model.Businesses;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
 public class YelpService {
+
+    @Value("yelpToken")
+    String yelpAPIKey;
+
+    public Businesses fetchYelpData(){
+    Businesses b = new Businesses();
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization",  yelpAPIKey);
+
+        return b;
+    }
 }
