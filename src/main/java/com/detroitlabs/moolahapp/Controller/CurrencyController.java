aad4344d0ecc49381170e02c1ed532b4939dade9
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-03-13 15:28:25 -0400

    Adds method to print first twenty results to the home page

--- src/main/java/com/detroitlabs/moolahapp/Controller/CurrencyController.java
+++ src/main/java/com/detroitlabs/moolahapp/Controller/CurrencyController.java
@@ -1,17 +1,25 @@
 package com.detroitlabs.moolahapp.Controller;
 
 import com.detroitlabs.moolahapp.Model.AllCurrencyData;
+import com.detroitlabs.moolahapp.Model.BusinessInformation;
+import com.detroitlabs.moolahapp.Model.Businesses;
 import com.detroitlabs.moolahapp.Services.CurrencyService;
+import com.detroitlabs.moolahapp.Services.YelpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
 
 @Controller
 public class CurrencyController {
 
     @Autowired
     CurrencyService currencyService;
+
+    @Autowired
+    YelpService yelpService;
 
     @RequestMapping("/")
     public ModelAndView showCurrencyInformation(){
@@ -20,6 +28,9 @@ public class CurrencyController {
        Double usdToAud= allCurrencyData.getQuotes().getUSDAUD();
        modelAndView.addObject("currency"
                , usdToAud);
+        Businesses businesses = yelpService.fetchYelpData();
+        ArrayList<BusinessInformation> businessInformationList = businesses.getAllYelpData();
+        modelAndView.addObject("businesses", businessInformationList);
 
         return modelAndView;
     }
